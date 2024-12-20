bc910f106c511ab37a03be04b55a1b8a9d1562ae
Mikhail Krasnov <mich.krs.01@gmail.com>
2024-12-20 22:04:00 +0300

    some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import styles from './App.module.scss'
-import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Section1 from './components/Section1/Section1'
 import Section2 from './components/Section2/Section2'
 import Section3 from './components/Section3/Section3'
 import Section4 from './components/Section4/Section4'
 import Section5 from './components/Section5/Section5'
-import Section6 from './components/Section6/Section6'
-import Section7 from './components/Section7/Section7'
 // TODO: вынести импорты в отдельный файл
 
 const App = () => {
